3545253683f419a5827695e77afc753f0076c996
Christian H S <christianfaldox@gmail.com>
2017-03-25 17:10:25 -0400

    listar cliente alteracoes

--- application/controllers/Base.php
+++ application/controllers/Base.php
@@ -45,18 +45,18 @@ class Base extends CI_Controller {
 
 	public function ListarClienteFace(){
 		// Recupera os contatos através do model
-		$facebook = $this->facebook_model->GetAll('nome');
+		$facebook = $this->contatos_model->GetAll('nome');
 		// Passa os contatos para o array que será enviado à home
-		$dados['contatos'] =$this->facebook_model->Formatar($facebook);
+		$dados['contatos'] =$this->contatos_model->Formatar($facebook);
 		// Chama a home enviando um array de dados a serem exibidos
 		$this->load->view('ListarCliente',$dados);
 	}
 
 	public function ListarClienteSite(){
 		// Recupera os contatos através do model
-		$site = $this->site_model->GetAll('nome');
+		$site = $this->contatos_model->GetAll('nome');
 		// Passa os contatos para o array que será enviado à home
-		$dados['contatos'] =$this->site_model->Formatar($site);
+		$dados['contatos'] =$this->contatos_model->Formatar($site);
 		// Chama a home enviando um array de dados a serem exibidos
 		$this->load->view('ListarCliente',$dados);
 	}
